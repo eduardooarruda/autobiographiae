c242112066c1dd1f55afa7fd471ee7f4380c9ef8
eduardooarruda <77410740+eduardooarruda@users.noreply.github.com>
2022-01-10 14:42:07 -0300

    adição da pagina de login

--- app.py
+++ app.py
@@ -11,4 +11,9 @@ def create_app():
         @app.route("/home")
         def hello_world():
             return render_template("index.html")
+        
+        @app.route("/login")
+        def login():
+            return render_template("login.html")
+            
     return app
