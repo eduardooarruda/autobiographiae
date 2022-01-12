a23f9dac555421f9fadd3645cfdf5fca68d2314c
eduardooarruda <77410740+eduardooarruda@users.noreply.github.com>
2022-01-12 19:44:15 -0300

    Adicão da funcionalidade de mostrar o preview da imagem de cadastro e o uso do url_for para mostrar os caminhos dos arquivos estaticos

--- autobiographiae/home/home.py
+++ autobiographiae/home/home.py
@@ -2,7 +2,8 @@ from flask import Blueprint, render_template
 
 bp = Blueprint("home", __name__, template_folder="templates")
 
+
 @bp.route("/")
 @bp.route("/home")
-def hello_world():
+def home():
     return render_template("index.html")
