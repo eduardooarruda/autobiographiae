d2373cb4214369ac976d17a4ff6922722d0ff7f0
eduardooarruda <77410740+eduardooarruda@users.noreply.github.com>
2022-01-17 16:58:49 -0300

    Adição do arquivo de configuração config.py

--- autobiographiae/app.py
+++ autobiographiae/app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
-import os
-SECRET_KEY = os.urandom(32)
+# import os
+# SECRET_KEY = os.urandom(32)
 
 def create_app():
 
     app = Flask(__name__)
 
+    if app.config["ENV"] == 'production':
+        app.config.from_object('config.ProductionConfig')
+    elif app.config["ENV"] == 'development':
+        app.config.from_object('config.DevelopmentConfig')
+
     with app.app_context():
-        app.config['SECRET_KEY'] = SECRET_KEY
+        # app.config['SECRET_KEY'] = SECRET_KEY
         
         from home import home
         app.register_blueprint(home.bp)
@@ -17,6 +22,6 @@ def create_app():
 
         from error_routes import page_not_found
         app.register_error_handler(404, page_not_found)
-        
+        # print(f'senha = {app.config["SECRET_KEY"]}')
 
     return app
---
+++ autobiographiae/config.py
@@ -0,0 +1,8 @@
+class Config(object):
+    SECRET_KEY = 'l@edk12WW'
+
+class ProductionConfig(Config):
+    pass
+
+class DevelopmentConfig(Config):
+    pass
