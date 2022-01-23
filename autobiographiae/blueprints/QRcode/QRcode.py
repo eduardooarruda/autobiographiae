84d460c6bc034892becdb67d9bd2a12d73b2fe57
eduardooarruda <77410740+eduardooarruda@users.noreply.github.com>
2022-01-23 17:48:41 -0300

    Adicao da pagina com o QRcode e  da logo

---
+++ autobiographiae/blueprints/QRcode/QRcode.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint('QRcode',__name__, url_prefix="/QRcode", template_folder="templates", static_folder="static", static_url_path='/static')
+
+@bp.route("/", methods=['GET', 'POST'])
+def qrcode():
+    return render_template("qrcode.html")
