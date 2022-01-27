a723ed759eb0a28b8d9f0ec1d5c4060b31583089
eduardooarruda <77410740+eduardooarruda@users.noreply.github.com>
2022-01-27 16:34:26 -0300

    Criei a função que vai pesquisar o usuario no banco de dados e a pagina para virsualizar a autobiografia

--- autobiographiae/blueprints/autobiografia/autobiografia.py
+++ autobiographiae/blueprints/autobiografia/autobiografia.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template, request, Response
 from flask_login import login_required, current_user
 from autobiographiae.models import Autobiografia, Usuario
 from autobiographiae.app import db
-import datetime
 
 bp = Blueprint('autobiografia', __name__,
                url_prefix="/autobiografia", template_folder="templates", static_folder="static")
@@ -22,3 +21,22 @@ def adicionar():
         db.session.commit()
         
     return render_template('addAutobiografia.html')
+
+@bp.post("/pesquisar-autobiografia")
+def pesquisar_autobiografia():
+    nome_usuario = request.form['nome_usuario']
+    pesquisa = f"%{nome_usuario}%"
+    lista_usuarios = Usuario.query.filter(Usuario.nome.like(pesquisa)).all() 
+    qtd_usuarios = len(lista_usuarios)
+    
+    if qtd_usuarios:
+        resultado = f"Resultado: {qtd_usuarios}" 
+        return render_template("lista_usuarios.html", lista_usuarios=lista_usuarios, resultado=resultado)
+    else:
+        resultado = "Nenhum resultado foi encontrado"
+        return render_template("lista_usuarios.html", resultado=resultado)
+
+@bp.get("/mostrar-autobiografia/<id_usuario>")
+def mostrar_autobiografia(id_usuario):
+    usuario = Usuario.query.filter_by(id=id_usuario).first()
+    return render_template("autobiografia.html", usuario=usuario)
